b547f45cdbcc795c2c484d289a4164e36acc1ff8
linyuansup <linyuansup@gmail.com>
2023-05-15 12:47:57 +0800

    优化测试

--- test_test.go
+++ test_test.go
@@ -9,6 +9,12 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	phoneNumber = "19556172642"
+	password = "81a0ad68ca3e7943db8833dc48927e2f"
+	deviceID = "5wi1RhQ#JMunWu_I"
+)
+
 func TestMain(m *testing.M) {
 	var err error
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", global.DatabaseAddress, global.DatabasePort, global.DatabaseUsername, global.DatabasePassword, global.DatabaseName+"_test")
--- user_test.go
+++ user_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestCreateUser(t *testing.T) {
 	res, err := api.User.Create(0, &request.UserCreateRequest{
-		PhoneNumber: "19556172642",
-		Password: "81a0ad68ca3e7943db8833dc48927e2f",
-		DeviceID: "5wi1RhQ#JMunWu_I",
+		PhoneNumber: phoneNumber,
+		Password: password,
+		DeviceID: deviceID,
 	}, context.Background())
 	if err != nil {
 		t.Logf("create user fail: %+v", err)
