4bf5274672c5d5d980617aad26e0355b99fdf6e4
linyuansup <linyuansup@gmail.com>
2023-05-15 12:53:43 +0800

    增加手机号鉴权

--- api/user.go
+++ api/user.go
@@ -14,12 +14,16 @@ type user struct{}
 var User user
 
 func (u *user) Create(uid int, request *request.UserCreateRequest, ctx context.Context) (*response.Response[response.UserCreateResponse], *errcode.Error) {
+	_, e := database.UserDatabase.FindByPhoneNumber(request.PhoneNumber, ctx)
+	if e.Code != errcode.NoUserFound.Code {
+		return nil, errcode.PhoneNumberAlreadyExist
+	}
 	targetUser := &entity.User{
 		PhoneNumber: request.PhoneNumber,
 		Password: request.Password,
 		DeviceID: request.DeviceID,
 	}
-	e := database.UserDatabase.Add(targetUser, ctx)
+	e = database.UserDatabase.Add(targetUser, ctx)
 	if e != nil {
 		return nil, e
 	}
--- errcode/userError.go
+++ errcode/userError.go
@@ -1,3 +1,5 @@
 package errcode
 
-var ()
+var (
+	PhoneNumberAlreadyExist = NewError(20000001, "手机号已存在", 400)
+)
