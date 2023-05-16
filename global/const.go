4a987790aa444279858ad2f18b5e1e6142c4d6a7
磷元素 <2021215159@mail.hfut.edu.cn>
2023-05-16 08:57:34 +0800

    更新文件 const.go

--- global/const.go
+++ global/const.go
@@ -3,7 +3,7 @@ package global
 var (
 	VersionKey       = "516^*ydj0DCZZ&EHjAEav^bw7Xt6_9MozZOIqA5RoklyJBU#q1yctzBONH&C1Ybh"
 	Version          = "v1.0.0"
-	DatabaseAddress  = "127.0.0.1"
+	DatabaseAddress  = "172.17.0.3"
 	DatabasePort     = "5432"
 	DatabaseUsername = "gaussdb"
 	DatabasePassword = "ZhangYinQi114514!"
